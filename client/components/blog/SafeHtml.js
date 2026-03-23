b615599a62b73dadddeeba75efd1b8b1333f25ac
Dikesh Ganboi <dgunboi@gmail.com>
2026-03-24 01:20:53 +0530

    fix: remove isomorphic-dompurify from client to fix Vercel SSR crash

--- client/components/blog/SafeHtml.js
+++ client/components/blog/SafeHtml.js
@@ -1,12 +1,11 @@
 'use client';
 
-import DOMPurify from 'isomorphic-dompurify';
-
 export default function SafeHtml({ html, className }) {
   return (
     <div
       className={className}
-      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html || '') }}
+      dangerouslySetInnerHTML={{ __html: html || '' }}
+      suppressHydrationWarning
     />
   );
 }
